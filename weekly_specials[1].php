d88e9318fa4228ac6b6eb4bf20237e86ced67804
ameliecw <110359480+ameliecw@users.noreply.github.com>
2022-10-02 20:25:44 +1300

    Added specials for all weeks

--- weekly_specials[1].php
+++ weekly_specials[1].php
@@ -11,19 +11,20 @@ if(isset($_GET['special_sel'])){
 $special_query = "SELECT week_specials.week_num, food.food_name, drink.drink_name, week_specials.availability, week_specials.new_price 
 FROM food, drink, week_specials 
 WHERE week_specials.food_id = food.food_id 
-AND week_specials.drink_id = drink.drink_id";
+AND week_specials.drink_id = drink.drink_id
+AND week_id = '"  .  $id  .  "'";
 
 $special_result = mysqli_query($dbcon, $special_query);
 $special_record = mysqli_fetch_assoc($special_result);
 
 /*specials query - Dropdown menu */
-$all_special_query = "SELECT week_num FROM week_specials";
+$all_special_query = "SELECT week_id, week_num FROM week_specials";
 $all_special_result = mysqli_query($dbcon, $all_special_query);
 $special_rows = mysqli_num_rows($all_special_result);
 
-/*Query to gather all weekly specials items*/
+/*Query to gather all weekly specials items
 $query_all_items = "SELECT * FROM week_specials ORDER BY week_num";
-$all_items_results = mysqli_query($dbcon,$query_all_items);
+$all_items_results = mysqli_query($dbcon,$query_all_items);*/
 
 ?>
 
@@ -31,10 +32,13 @@ $all_items_results = mysqli_query($dbcon,$query_all_items);
 
 <html lang="en">
 <head>
-	<title> Cafe Specials</title>
+	<title> WEGC café</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, inital scale=1.0">
 	<link rel='stylesheet' type='text/css' href='style.css'>
-	<link href="https://fonts.googleapis.com/css2?family=Fjord+One&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 	
 <main>
@@ -42,7 +46,8 @@ $all_items_results = mysqli_query($dbcon,$query_all_items);
 	<div class="grid-container">
 		<div class="item1">
 			<header>
-				<h1>Weekly specials :)</h1>
+				<img src="WEGC_logo.jpg" alt="WEGC logo" height="100fr" width="auto">
+				<h1>Wellington East Girls' College Café</h1>
 			</header>
 			</div>
 
@@ -58,44 +63,47 @@ $all_items_results = mysqli_query($dbcon,$query_all_items);
 		</div>
 
 		<div class="item3">
-			Intro Intro Intro Intro Intro Intro Intro Intro
+			<p>Specials are offered at the café weekly. Each special is a food and drink combo.</p>
+		</div>
+		
+		<div class="item4">
+			<h2>Weekly Specials</h2>
+			<?php
+			echo "<p> Week number: " . $special_record['week_num'] . "<br>";
+			echo "<p> Food: " . $special_record['food_name'] . "<br>";
+			echo "<p> Drink: " . $special_record['drink_name'] . "<br>";
+			echo "<p> Status: " . $special_record['availability'] . "<br>";
+			echo "<p> Price: $" . $special_record['new_price'] . "<br>";
+			?>
+			<br>
+
+
+			
 		</div>
 		
 		<div class="item5">
-			<!-- Dropdown specials form -->
-			<h2>Select Another Special</h2>
+			<!-- Dropdown food form -->
+			<h2>Select Another Food</h2>
 			<form name='special_form' id='special_form' method='get' action='weekly_specials.php'>
 				<!-- Dropdown menu -->
 				<select id='special_sel' name='special_sel'>
 					<!-- Options -->
 					<?php
 					while($all_special_record = mysqli_fetch_assoc($all_special_result)){
-						echo "<option value = '". $all_special_record['week_num'] . "'>";
+						echo "<option value = '". $all_special_record['week_id'] . "'>";
 						echo $all_special_record['week_num'];
 						echo "</option>";
 					}
 					?>
 				</select>
 
-				<input type='submit' name='special_button' value='See the specials'>
+				<input type='submit' name='food_button' value='See the food information'>
 			</form>	
-		</div>
-
-		<div class="item4">
-			<h2>Weekly information</h2>
-			<?php
-			echo "<p> Week number: " . $special_record['week_num'] . "<br>";
-			echo "<p> Food item: " . $special_record['food_name'] . "<br>";
-			echo "<p> Drink item: " . $special_record['drink_name'] . "<br>";
-			echo "<p> Status: " . $special_record['availability'] . "<br>";
-			echo "<p> Cost: $" . $special_record['new_price'] . "<br>";
-			?>
-
 		</div>
 
 		<div class="item6">
 			<footer>
-				<p>&copy; 2022 A Chow Worn.</p>
+				<p>&copy; 2022 A Chow Worn. School logo from Wellington East Girls' College.</p>
 			</footer>
 		</div>
 		
